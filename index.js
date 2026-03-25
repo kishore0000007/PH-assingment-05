4d09ca7e0292bd7f2671482186dc2c10c06c7879
kishore0000007 <kishoreroy938@gmail.com>
2026-03-25 09:52:42 +0600

    Done Login

---
+++ index.js
@@ -0,0 +1,17 @@
+
+ /*document.getElementById("login_form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (username === "admin" && password === "admin123") {
+        // Hide login
+        document.getElementById("login_page").classList.add("hidden");
+
+        // Show dashboard
+        document.getElementById("dashboard").classList.remove("hidden");
+    } else {
+        alert("Invalid Username or Password ");
+    }
+});*/
